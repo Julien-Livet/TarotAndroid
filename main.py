851579880f59d625f98a6f6d8a7b236b4814fb6a
Julien <julien.livet@free.fr>
2025-04-07 20:33:25 +0200

    Update

---
+++ main.py
@@ -0,0 +1,30 @@
+from client import Window
+import locale
+import os
+import gettext
+import kivy.app
+
+class App(kivy.app.App):
+    def build(self):
+        kivy.core.window.Window.orientation = 'landscape'
+
+        self._window = Window.Window(self)
+
+        return self._window
+
+if (__name__ == "__main__"):
+    locale_dir = os.path.join(os.path.dirname(__file__), 'locales')
+    lang = locale.getlocale()[0]
+    
+    from jnius import autoclass
+    Locale = autoclass('java.util.Locale')
+    loc = Locale.getDefault()
+    lang = loc.getLanguage() + "_" + loc.getCountry()
+
+    lang = gettext.translation('messages', localedir = locale_dir, languages = [lang], fallback = True)
+    lang.install()
+
+    app = App()
+    app.run()
+
+    app._window.close()
