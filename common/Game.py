32a18e6f60e15287b09bded4e3d1a5a75c7dd65e
Julien <julien.livet@free.fr>
2025-04-13 19:43:48 +0200

    Update

--- common/Game.py
+++ common/Game.py
@@ -337,13 +337,13 @@ class GameData:
             
             if (i == self._currentPlayer):
                 draw = ImageDraw.Draw(tableImage)
-                draw.arc((int(avatarCenter[0]) - size[0] * 1.1 // 2,
-                          int(avatarCenter[1]) - size[1] * 1.1 // 2,
-                          int(avatarCenter[0]) + size[0] * 1.1 // 2,
-                          int(avatarCenter[1]) + size[1] * 1.1 // 2),
+                draw.arc((avatarCenter[0] - size[0] * 1.1 // 2,
+                          avatarCenter[1] - size[1] * 1.1 // 2,
+                          avatarCenter[0] + size[0] * 1.1 // 2,
+                          avatarCenter[1] + size[1] * 1.1 // 2),
                           start = -window._remainingTime / 15.0 * 360.0 + -90.0,
                           end = -90.0,
-                          fill = "green", width = 4 * factorSize)
+                          fill = "green", width = int(4 * factorSize))
 
             if (self._players[i].teamKnown()):
                 img = Image.open(os.path.dirname(__file__) + "/../images/shield.png")
